cadfb01eeb6e49bc0e9dfe08204cc078f6f11f5b
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-04-27 11:47:20 +0100

    basic project

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,46 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        //Defining the API
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        //Defining the client
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                 ClientId = "client",
+
+                 // no interactive user, use the clientid/secret for authentication
+                 AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                 // secret for authentication
+                 ClientSecrets =
+                 {
+                     new Secret("secret".Sha256())
+                  },
+
+                 // scopes that client has access to
+                 AllowedScopes = { "api1" }
+                }
+            };
+        }
+
+        //
+    }
+}
